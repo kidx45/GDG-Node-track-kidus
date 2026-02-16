187ddc9909131eee5744716331a01ccccead38dc
kidx45 <kidustessema91@gmail.com>
2026-02-16 14:17:25 +0300

    Fix-And-Nodemon-Addition

--- task1/task1.js
+++ task1/task1.js
@@ -1,94 +1,90 @@
-const http = require('http');
+const http = require("http");
 
 let students = [];
 
-const server = http.createServer((req,res)=>{
-    if (req.url == "/" && req.method == "GET") {
-        res.statusCode = 200;
-        
-        res.end("Welcome to Home Page");
-    } else if (req.url == "/info" && req.method == "GET") {
-        res.statusCode = 200;
-        
-        res.end("This is the information page")
-    } else if (req.url == "/submit" && req.method == "POST") {
-        res.statusCode = 201;
+const server = http.createServer((req, res) => {
+  if (req.url == "/" && req.method == "GET") {
+    res.statusCode = 200;
 
-        req.on('data', (body) => {
-            res.setHeader("Content-Type","application/json")
-            res.end(JSON.stringify(JSON.parse(body)));
-        })
+    res.end("Welcome to Home Page");
+  } else if (req.url == "/info" && req.method == "GET") {
+    res.statusCode = 200;
 
-    } else if (req.url == "/students" && req.method == "GET"){
-        req.statusCode = 200;
-        
-        res.setHeader("Content-Type","application/json")
-        
-        res.end(JSON.stringify(students))
-    } else if (req.url == "/students" && req.method == "POST"){
-        res.statusCode = 201;
-        
-        req.on('data', (body)=>{
-            let student = {
-                id: JSON.parse(body).id,
-                name: JSON.parse(body).name
-            };
-            
-            students.push(student);
-            res.end("Added");
-        
-        })
-    } else if (req.method === "PUT") {
-        if (students.length == 0) {
-            res.end("Array empty");
-            return;
-        }
-        
-        const url = req.url.toString().split("/");
-        
-        if (url[1] === "students") {
-            const index = students.findIndex(s => s.id == url[2]);
-        
-            req.on('data',(body)=>{
-                if (index == -1) {
-                    res.statusCode = 404;
-                    res.end("Student not found");
-                    return;
-                }
+    res.end("This is the information page");
+  } else if (req.url == "/submit" && req.method == "POST") {
+    res.statusCode = 201;
 
-                students[index] = {
-                    id: url[2],
-                    name: JSON.parse(body).name
-                }
+    req.on("data", (body) => {
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(JSON.parse(body)));
+    });
+  } else if (req.url == "/students" && req.method == "GET") {
+    req.statusCode = 200;
 
-                res.end("Updated");
-            })
-        }
+    res.setHeader("Content-Type", "application/json");
 
-    } else if (req.method == "DELETE") {
-        if (students.length == 0) {
-            res.end("Array empty");
-            return;
-        }
-        
-        const url = req.url.toString().split("/");
-        
-        if (url[1] === "students") {
-            const index = students.findIndex(s => s.id == url[2]);
-                if (index == -1) {
-                    res.statusCode = 404;
-                    res.end("Student not found");
-                    return;
-                }
-                students.splice(index,1);
-                res.end("deleted");
-        }
+    res.end(JSON.stringify(students));
+  } else if (req.url == "/students" && req.method == "POST") {
+    res.statusCode = 201;
 
-    } else {
-        res.statusCode = 404;
+    req.on("data", (body) => {
+      let student = {
+        id: JSON.parse(body).id,
+        name: JSON.parse(body).name,
+      };
+
+      students.push(student);
+      res.end("Added");
+    });
+  } else if (req.method === "PUT") {
+    if (students.length == 0) {
+      res.end("Array empty");
+      return;
     }
-})
 
-server.listen(3000, ()=>{
-    console.log("Running ...")
-})
+    const url = req.url.toString().split("/");
+
+    if (url[1] === "students") {
+      const index = students.findIndex((s) => s.id == url[2]);
+
+      req.on("data", (body) => {
+        if (index == -1) {
+          res.statusCode = 404;
+          res.end("Student not found");
+          return;
+        }
+
+        students[index] = {
+          id: url[2],
+          name: JSON.parse(body).name,
+        };
+
+        res.end("Updated");
+      });
+    }
+  } else if (req.method == "DELETE") {
+    if (students.length == 0) {
+      res.end("Array empty");
+      return;
+    }
+
+    const url = req.url.toString().split("/");
+
+    if (url[1] === "students") {
+      const index = students.findIndex((s) => s.id == url[2]);
+      if (index == -1) {
+        res.statusCode = 404;
+        res.end("Student not found");
+        return;
+      }
+      students.splice(index, 1);
+      res.end("deleted");
+    }
+  } else {
+    res.statusCode = 404;
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Running ...");
+});
--- task2/task2.js
+++ task2/task2.js
@@ -1,26 +1,29 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res)=>{
-    res.status(200).type('.html').send('<p style="color: green;">Welcome to the page</p>')
-})
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .type(".html")
+    .send('<p style="color: green;">Welcome to the page</p>');
+});
 
-app.get('/about', (req,res)=>{
-    res.status(200).type('plain').send(`
+app.get("/about", (req, res) => {
+  res.status(200).type("plain").send(`
 This page consists information about how the page is constructed.
-It is done using Express and return a plain text as response.`)
-})
+It is done using Express and return a plain text as response.`);
+});
 
-app.get('/students/:studentid', (req,res)=>{
-    var result = {
-        Query: req.query.department,
-        Param: req.params.studentid
-    }
-    res.status(200).json(result)
-})
+app.get("/students/:studentId", (req, res) => {
+  var result = {
+    Query: req.query.department,
+    Param: req.params.studentId,
+  };
+  res.status(200).json(result);
+});
 
-app.listen(port, ()=>{
-    console.log("Running...");
-})
+app.listen(port, () => {
+  console.log("Running...");
+});
