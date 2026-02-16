826b98d79eff9456b378ac6b0fa57e3c189c31f6
kidx45 <kidustessema91@gmail.com>
2026-02-16 14:00:39 +0300

    Fix-App-File-And-Install-Correct-Library

--- task3/src/app.js
+++ task3/src/app.js
@@ -1,10 +1,10 @@
-import express from 'express'
-import * as routes from './routes/bookRoutes.js'
-import * as errorhander from './middleware/errorHandler.js'
-import morgan from 'morgan'
+import express from "express";
+import * as routes from "./routes/bookRoutes.js";
+import * as errorhander from "./middleware/errorHandler.js";
+import morgan from "morgan";
 
-export const app = express()
-app.use(morgan('dev'))
-app.use(express.json())
-app.use('/books',routes.router)
-app.use(errorhander.errorHandler)
+export const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/books", routes.router);
+app.use(errorhander.errorHandler);
